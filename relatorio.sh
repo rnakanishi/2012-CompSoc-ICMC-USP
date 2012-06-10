2d2dea8517318debfc8522fbaf19bdd668240c75
Bianca Oe <biancamsoe@gmail.com>
2012-06-09 21:21:32 -0300

    Relatorio

--- relatorio.sh
+++ relatorio.sh
@@ -1,1 +1,5 @@
-
+rm relatorio.{glo,gls,ist,log,out,pdf,aux,glg,toc}
+pdflatex relatorio.tex
+makeindex relatorio.glo -s relatorio.ist -t relatorio.glg -o relatorio.gls
+pdflatex relatorio.tex
+rm relatorio.{glo,gls,ist,log,out,aux,glg,toc}
