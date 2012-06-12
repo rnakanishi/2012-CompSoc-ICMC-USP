d932f187d51bfd592551d7a64465835d0e655226
Bianca Oe <biancamsoe@gmail.com>
2012-06-12 00:06:56 -0300

    Monografia

--- relatorio.sh
+++ relatorio.sh
@@ -1,8 +1,10 @@
 #rm relatorio.{glo,gls,ist,log,out,pdf,aux,glg,toc}
-latex relatorio.tex
-bibtex relatorio.aux
-makeindex relatorio.glo -s relatorio.ist -t relatorio.glg -o relatorio.gls
-latex relatorio.tex
-latex relatorio.tex
+latex relatorio
+bibtex relatorio
+#makeindex relatorio.glo -s relatorio.ist -t relatorio.glg -o relatorio.gls
+makeglossaries relatorio
+latex relatorio
+latex relatorio
 dvipdfm relatorio.dvi
-rm relatorio.{glo,gls,ist,log,out,aux,glg,toc}
+rm relatorio.{glo,gls,ist,log,aux,glg,toc,bbl,blg,dvi}
+evince relatorio.pdf
